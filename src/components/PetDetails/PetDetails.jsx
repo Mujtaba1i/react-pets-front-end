d603db1f3f12834ed21f4a2294ff0362d9d44892
Mujtaba1i <unmujt@gmail.com>
2025-12-30 11:07:11 +0300

    quick commit

--- src/components/PetDetails/PetDetails.jsx
+++ src/components/PetDetails/PetDetails.jsx
@@ -1,13 +1,28 @@
+import { useEffect,useState } from "react"
 import { show } from "../../services/petService"
+import { useParams } from "react-router"
 
 function PetDetails() {
-    const onePet = show()
+    const [onePet, setOnePet] = useState(null)
+    const {id} = useParams()
+
+    async function showFcn(id){
+        const onePet = await show(id)
+        setOnePet(onePet)
+    }
+
+    useEffect(() => {
+        showFcn(id)
+    }, [id])
+
+    // if(!id) return <h1>Loading ...</h1>
+    if(!onePet) return <h1>Loading ...</h1>
 
   return (
     <div>
         <h1>Pet Details</h1>
         <div>
-            <p>Name: {onePet.name}, Age: {onePet.age}, Breed: {onePet.breed}</p>
+            <p>Name: {onePet.name} | Age: {onePet.age} | Breed: {onePet.breed}</p>
         </div>
     </div>
   )
