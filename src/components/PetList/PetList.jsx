5c9e9518371c6a9610535261c54c3a5b3c870608
Mujtaba1i <unmujt@gmail.com>
2025-12-30 08:34:09 +0300

    quick commit

--- src/components/PetList/PetList.jsx
+++ src/components/PetList/PetList.jsx
@@ -2,17 +2,19 @@
 function PetList({pets}) {
   return (
     <div>
-        <h1>Pet List</h1>
+    <h1>Pet List</h1>
         <div>
-            <ul>
-                {pets.map(onePet=>
-                (<li key={onePet._id}>
-                    {onePet.name}
-                </li>))}
-            </ul>
-        </div>        
-    </div>
+            {!pets.length ? (<h2>No Pets Yet</h2>) :
+                (<ul>
+                    {pets.map(onePet=>
+                    <li key={onePet._id}>
+                        {onePet.name}
+                    </li>)}
+                </ul>)}
+            </div>
+        </div>
   )
 }
+
 
 export default PetList
